5699fe65d96552d1eb213cda22a9490885d6c9a5
Ana Tulea <anatulea14@gmail.com>
2021-01-08 19:18:59 -0800

    inserting database records using forms

--- app/catalog/routes.py
+++ app/catalog/routes.py
@@ -3,7 +3,7 @@ from app import db
 from app.catalog.models import Book, Publication
 from flask import render_template, flash, request, redirect, url_for
 from flask_login import login_required
-from app.catalog.forms import EditBookForm
+from app.catalog.forms import EditBookForm, CreateBookForm
 
 
 @main.route('/')
@@ -47,3 +47,23 @@ def edit_book(book_id):
         flash('Book Edited Successfully')
         return redirect(url_for('main.display_books'))
     return render_template('edit_book.html', form=form)
+
+
+@main.route('/create/book/<pub_id>', methods=['GET', 'POST'])
+@login_required
+def create_book(pub_id):
+
+    form = CreateBookForm()
+    form.pub_id.data = pub_id  # pre-populates pub_id
+
+    if form.validate_on_submit():
+        book = Book(title=form.title.data, author=form.author.data, avg_rating=form.avg_rating.data,
+                    book_format=form.format.data, image=form.img_url.data, num_pages=form.num_pages.data,
+                    pub_id=form.pub_id.data)
+
+        db.session.add(book)
+        db.session.commit()
+        flash('Book added successfully')
+        return redirect(url_for('main.display_publisher', publisher_id=pub_id))
+
+    return render_template('create_book.html', form=form, pub_id=pub_id)
