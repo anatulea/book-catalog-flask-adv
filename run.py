b0cd17f703ff45cdfb72e6a5009c9d0fedc9fad1
Ana Tulea <anatulea14@gmail.com>
2021-01-08 20:44:22 -0800

    added heroku files

--- run.py
+++ run.py
@@ -2,7 +2,7 @@ from app import create_app, db
 from app.auth.models import User
 
 if __name__ == "__main__":
-    flask_app = create_app('dev')
+    flask_app = create_app('prod')
     with flask_app.app_context(): # to ensure that the SQLAlchemy binds with my application(there may be multiple apps)
         db.create_all()
 
