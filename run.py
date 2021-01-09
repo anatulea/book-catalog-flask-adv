b2c1b0cdec04570d141ff662c81aa137520377d0
Ana Tulea <anatulea14@gmail.com>
2021-01-08 16:21:06 -0800

    added app context

--- run.py
+++ run.py
@@ -1,5 +1,7 @@
-from app import create_app
+from app import create_app, db
 
 if __name__ == "__main__":
     flask_app = create_app('dev')
+    with flask_app.app_context(): # to ensure that the SQLAlchemy binds with my application(there may be multiple apps)
+        db.create_all()
     flask_app.run(debug=True)
