7ab8534fd1b97c04ac8a8670e2fae3fadeed4063
Ana Tulea <anatulea14@gmail.com>
2021-01-08 16:53:15 -0800

    displaying publisher  data in a browser

--- app/catalog/routes.py
+++ app/catalog/routes.py
@@ -8,3 +8,12 @@ from flask import render_template, flash, request, redirect, url_for
 def display_books():
     books = Book.query.all()
     return render_template('home.html', books=books)
+
+
+@main.route('/display/publisher/<publisher_id>')
+def display_publisher(publisher_id):
+    publisher = Publication.query.filter_by(id=publisher_id).first()
+    publisher_books = Book.query.filter_by(pub_id=publisher.id).all()
+    return render_template('publisher.html',
+                           publisher=publisher,
+                           publisher_books=publisher_books)
