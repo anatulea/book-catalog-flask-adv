68a5f5983f8ca6ddfd6dbca04fd8c3da610f982a
Ana Tulea <anatulea14@gmail.com>
2021-01-08 19:10:21 -0800

    update  database records

--- app/catalog/routes.py
+++ app/catalog/routes.py
@@ -3,6 +3,7 @@ from app import db
 from app.catalog.models import Book, Publication
 from flask import render_template, flash, request, redirect, url_for
 from flask_login import login_required
+from app.catalog.forms import EditBookForm
 
 
 @main.route('/')
@@ -30,3 +31,19 @@ def delete_book(book_id):
         flash('Book delete successfully')
         return redirect(url_for('main.display_books'))
     return render_template('delete_book.html', book=book, book_id=book.id)
+
+
+@main.route('/edit/book/<book_id>', methods=['GET', 'POST'])
+@login_required
+def edit_book(book_id):
+    book = Book.query.get(book_id)
+    form = EditBookForm(obj=book)
+    if form.validate_on_submit():  # if post request
+        book.title = form.title.data
+        book.format = form.format.data
+        book.num_pages = form.num_pages.data
+        db.session.add(book)
+        db.session.commit()
+        flash('Book Edited Successfully')
+        return redirect(url_for('main.display_books'))
+    return render_template('edit_book.html', form=form)
