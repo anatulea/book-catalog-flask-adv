5e8de8cde99b163d2089242bdee5c46343cbe6f5
Ana Tulea <anatulea14@gmail.com>
2021-01-08 17:43:23 -0800

    refactored registration form

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -1,8 +1,12 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
+from wtforms import StringField, SubmitField, PasswordField
+from wtforms.validators import DataRequired, Length, Email, EqualTo
 
 
 class RegistrationForm(FlaskForm):
-    name = StringField('Name')
-    email = StringField('E-mail')
+    name = StringField('Name', validators=[DataRequired(), Length(3, 15, message='Between 3 to 15 characters')])
+    email = StringField('E-mail', validators=[DataRequired(), Email()])
+    password = PasswordField('Password',
+                             validators=[DataRequired(), Length(5), EqualTo('confirm', message='Password must match')])
+    confirm = PasswordField('Confirm', validators=[DataRequired()])
     submit = SubmitField('Register')
